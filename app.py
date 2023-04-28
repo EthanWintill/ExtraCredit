5ba6502a4d85f8e44ffb8e53a98a05706b1e5a38
EW <96669967+bigbrrrr@users.noreply.github.com>
2023-04-28 00:58:06 -0500

    done!

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return render_template('badpage.html')
+
+@app.route('/lottery')
+def lotter():
+    return render_template('lotter.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
